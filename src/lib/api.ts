40f3168eecc74d48cb780bb091bc606acdbb6278
alideeb-coder <alideeb566056@gmail.com>
2026-09-24 09:08:37 +0300

    edit links

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from 'axios';
 const TOKEN_KEY = 'taskflow_token';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
